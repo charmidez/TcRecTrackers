bae098f78a689668a492e26af8f7d113dc0fe6e4
Charmidez Assiobo <kodjoassiobo@protonmail.com>
2023-08-04 14:57:42 +0000

    Mode sombre annulé pour l'instant

--- app/src/main/java/com/charmidezassiobo/tcrec/ui/ajoutertc/AjoutertcFragment.kt
+++ app/src/main/java/com/charmidezassiobo/tcrec/ui/ajoutertc/AjoutertcFragment.kt
@@ -7,8 +7,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.RadioButton
