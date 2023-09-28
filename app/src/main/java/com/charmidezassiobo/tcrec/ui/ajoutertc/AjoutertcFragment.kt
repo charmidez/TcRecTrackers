94fe60a018fe95d81e7b410b496292d4624b8dbc
Charmidez Assiobo <kodjoassiobo@protonmail.com>
2023-09-28 17:15:15 +0000

    Revert "Mode sombre annulé pour l'instant"
    This reverts commit bae098f78a689668a492e26af8f7d113dc0fe6e4.

--- app/src/main/java/com/charmidezassiobo/tcrec/ui/ajoutertc/AjoutertcFragment.kt
+++ app/src/main/java/com/charmidezassiobo/tcrec/ui/ajoutertc/AjoutertcFragment.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.RadioButton
